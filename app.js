084ec43588372eaee776be8de72163f8bacac0b0
Alan McIvor <alan.mcivor27@gmail.com>
2023-03-17 19:40:09 +0000

    small changes to margins

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+// variables
 const btn = document.getElementById('menu-btn' );
 const ul = document.querySelector('ul')
 const overlay = document.getElementById('overlay');
 
+// when hamburger is clicked either open or close, apply overlay and stop scrolling
+
+// event listener for hamburger menu
 btn.addEventListener('click', navToggle);
 
+// hamburger open and close function, stop scrolling and overlay open
 function navToggle(){
     btn.classList.toggle('open');
     ul.classList.toggle('show-menu')
     overlay.classList.toggle('overlay-show');
     document.body.classList.toggle('stop-scrolling')
+}
+
+// when arrow is clicked scroll down
+
+// variable
+const arrow = document.getElementById('arrow');
+
+// event listener for arrow scroll
+arrow.addEventListener('click', scrollTo);
+
+function scrollTo(){
+    document.getElementById('about').scrollIntoView({behavior: "smooth", block: "center"})
 }
